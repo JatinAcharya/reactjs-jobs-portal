8e703a9236e7a19d3cb28d83d9c641cc9fa3217f
jatin.acharya.ext <jatin.acharya.ext@veolia.com>
2025-06-07 23:24:23 +0530

    navigation configured with the existing components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -9,35 +10,32 @@ const Navbar = () => {
           <div class="flex h-20 items-center justify-between">
             <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
               {/* <!-- Logo --> */}
-              <a
-                class="flex flex-shrink-0 items-center mr-4"
-                href="/index.html"
-              >
+              <Link class="flex flex-shrink-0 items-center mr-4" to="/">
                 <img class="h-10 w-auto" src={logo} alt="React Jobs" />
                 <span class="hidden md:block text-white text-2xl font-bold ml-2">
                   React Jobs
                 </span>
-              </a>
+              </Link>
               <div class="md:ml-auto">
                 <div class="flex space-x-2">
-                  <a
-                    href="/index.html"
+                  <Link
+                    to="/"
                     class="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Home
-                  </a>
-                  <a
-                    href="/jobs.html"
+                  </Link>
+                  <Link
+                    to="/jobs"
                     class="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Jobs
-                  </a>
-                  <a
-                    href="/add-job.html"
+                  </Link>
+                  <Link
+                    to="/add-job"
                     class="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >
                     Add Job
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
