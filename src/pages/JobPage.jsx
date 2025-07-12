45bec1e6f95c15cf578ac5881d3fea306396b6ea
jatin.acharya.ext <jatin.acharya.ext@veolia.com>
2025-07-12 16:47:33 +0530

    delete functionality added

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 // import { useState, useEffect } from "react";
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
   // const [job, setJob] = useState(null);
   // const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   const job = useLoaderData();
 
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this listing?"
+    );
+
+    if (!confirm) {
+      return;
+    }
+    deleteJob(jobId);
+    navigate("/jobs");
+  };
   //fetching using useEffect
   // useEffect(() => {
   //   const fetchJob = async () => {
@@ -88,7 +100,10 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
